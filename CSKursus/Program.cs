1d478223e046710392918d3f8f8c019b3b959945
admin <admin@TA-8213-02>
2018-11-13 15:01:23 +0100

    Add project files.

---
+++ CSKursus/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Start på op tælling:");
+
+            //LøkkeSomMedtode(1, 20);
+
+            PathFinder(@"c:/");
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+        public static void LøkkeSomMedtode(int fra, int til)
+        {
+
+            Console.WriteLine(fra);
+
+            if (fra < til)
+            {
+                LøkkeSomMedtode(fra + 1, til);
+            }
+        }
+
+
+
+        public static void PathFinder(string path)
+        {
+
+            DirectoryInfo d = new DirectoryInfo(path);
+            
+
+
+                FileInfo[] files = d.GetFiles();
+                DirectoryInfo[] Folders = d.GetDirectories();
+ 
+
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+
+            foreach (var folder in Folders)
+            {
+                PathFinder(folder.FullName);
+            }
+
+        }
+
+    }
+
+
+
+
+}
+
+
+
---
+++ Terning/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Spil = new Yatzy();
+
+
+            while (true)
+            {
+                Spil.ClearDisplay();
+                Spil.KastTerning();
+                Spil.Skriv();
+                for (int i = 0; i < 3; i++)
+                {
+                    if (Spil.CheckMatch())
+
+                    {
+                        i = 4;
+                    }
+
+                    if (i < 2)
+                    {
+                        Spil.Behold();
+                        Spil.Skriv();
+
+                    }
+                    
+                }
+
+                
+            }
+
+
+
+
+            Console.WriteLine("");
+
+
+            //Check for mulige kombinationer og giv brugeren et valg om at holde bestemte terninger
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
